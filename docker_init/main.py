9d1a3e6f132dfadb54ebcd2cbe559a6423552f56
S Gleb <igamgam97@gmail.com>
2019-12-16 03:15:32 +0300

    i hate this shirt

--- docker_init/main.py
+++ docker_init/main.py
@@ -1,27 +1,39 @@
 import logging
 import sys
+import os
 import subprocess
-from typing import Dict
+import json
+from typing import Dict, List
+
+from azure.servicebus import QueueClient, Message
 
 
-def get_meggase(channel: str) -> Dict[str, str]:
-    raise NotImplementedError()
+def get_message(channel: str, conn_string: str) -> Dict[str, str]:
+    q = QueueClient.from_connection_string(conn_string, channel)
+    with q.get_receiver() as qr:
+        messages = qr.fetch_next(timeout=30)
+        message = str(messages[0])
+        json_message = json.loads(message)
+        return json_message
 
 
-def run_sh(command: str) -> str:
-    result = subprocess.run([command], capture_output=True)
+def run_sh(command: List[str]) -> str:
+    result = subprocess.run(command, capture_output=True)
     if result.stdout:
         return result.stdout.decode("utf-8")
     raise Exception()
 
 
-def send_message(channel: str, out_message):
-    raise NotImplementedError()
+def send_message(channel: str, out_message: Dict[str, str], conn_string: str):
+    q = QueueClient.from_connection_string(conn_string, channel)
+    json_message = Message(json.dumps(out_message).encode("utf-8"))
+    q.send(json_message)
 
 
 if __name__ == "__main__":
-    channel = ""
-    message = get_meggase(channel)
+    channel = "incoming"
+    conn_string: str = os.getenv("CONN_STRING")
+    message = get_message(channel, conn_string)
     try:
         out_channel_name = message["auth_uid"]
         repo = message["repo"]
@@ -29,13 +41,30 @@ if __name__ == "__main__":
         logging.error("no expected fields in request message")
         sys.exit(1)
 
-    command = f"/usr/bin/git clone {repo} /src"
+    command = ["/usr/bin/git", "clone", f"{repo}", "/src"]
     try:
-        result = run_sh(command)
+        run_sh(command)
     except Exception:
         logging.error("something wrong in bash command")
         sys.exit(1)
 
-    out_message = {"result": result}
-    send_message(out_channel_name, out_message)
+    command = [
+        "java",
+        "-jar",
+        "/detekt/detekt-cli-1.2.1-all.jar",
+        "-i",
+        "/src",
+        "-r",
+        "html:/result.html",
+    ]
+    try:
+        run_sh(command)
+    except Exception:
+        logging.error("something wrong in bash command")
+        sys.exit(1)
+
+    with open("/result.html") as html:
+        result = html.read()
+        out_message = {"result": result}
+        send_message(out_channel_name, out_message, conn_string)
     sys.exit(0)
--- gateway/app.py
+++ gateway/app.py
@@ -1,39 +1,57 @@
 import json
-
+import logging
+import sys
+import os
+import subprocess
 from aiohttp import web
 from aiohttp.web import (
     Request,
-    Response,
     HTTPBadRequest,
     HTTPForbidden,
     WebSocketResponse,
 )
+import uuid
 
-from typing import Tuple, Dict
+from azure.servicebus import ServiceBusClient, QueueClient, Message
+
+from typing import Tuple, Dict, List
 
 
 async def handle(request: Request):
     auth_uid, repo = await parse_params(request)
-    if auth_uid not in request.app.config["auth_users"]:  # type: ignore
+    if auth_uid != "jiGVe0bRMBeo1BpYme0BjTiD2pC2":  # type: ignore
         raise HTTPForbidden()
 
     base_user_dict: Dict[str, str] = {
         "user_uid": auth_uid,
         "repo_addr": repo,
     }
+    conn_string: str = os.getenv("CONN_STRING")
     ws: WebSocketResponse = WebSocketResponse()
     await ws.prepare(request)
-    await send_to_mq(auth_uid, json.dumps(base_user_dict))
-    await run_ansible_start_container(auth_uid, repo)
 
-    await mq_pooler(ws, auth_uid)
+    ws.send_str(json.dumps({"status": "CREATE QUEUE..."}))
+    create_recieve_queue(auth_uid, conn_string)
+
+    ws.send_str(json.dumps({"status": "SENDING MESSAGE..."}))
+    send_to_mq(auth_uid, base_user_dict, conn_string)
+
+    random_str = str(uuid.uuid4())
+
+    ws.send_str(json.dumps({"status": "CREATE CONTAINER FOR YOU..."}))
+    run_ansible_start_container(conn_string, random_str)
+
+    ws.send_str(json.dumps({"status": "PROCESSING..."}))
+    result = await wait_result(auth_uid, conn_string)
+
+    ws.send_str(json.dumps({"status": "RESULT READY. DELETE CONTAINER..."}))
+    delete_receive_queue(auth_uid, conn_string)
+    run_ansible_destroy_container(random_str)
+
+    ws.send_str(json.dumps({"status": "DONE"}.update(result)))
 
     ws.close()
     return ws
-
-
-async def send_frontend(request: Request) -> Response:
-    raise NotImplementedError()
 
 
 async def parse_params(request: Request) -> Tuple[str, str]:
@@ -45,35 +63,75 @@ async def parse_params(request: Request) -> Tuple[str, str]:
         raise HTTPBadRequest(text="No options auth_uid or repo!")
 
 
-async def run_ansible_start_container(auth_uid: str, repo_addr: str):
-    raise NotImplementedError()
+async def run_ansible_start_container(conn_string: str, rand: str):
+    command = [
+        "ansible-playbook",
+        "-i",
+        "/app/inventory.yml",
+        "/app/create_web_app.yml",
+        "--extra-vars",
+        f"conn_string={conn_string} random={rand}",
+    ]
+    try:
+        run_sh(command)
+    except Exception:
+        logging.error("something wrong in bash command")
+        sys.exit(1)
 
 
-async def run_ansible_destroy_container():
-    raise NotImplementedError()
+async def run_ansible_destroy_container(rand: str):
+    command = [
+        "ansible-playbook",
+        "-i",
+        "/app/inventory.yml",
+        "/app/destroy_web_app.yml",
+        "--extra-vars",
+        f"random={rand}",
+    ]
+    try:
+        run_sh(command)
+    except Exception:
+        logging.error("something wrong in bash command")
+        sys.exit(1)
 
 
-async def send_to_mq(auth_uid: str, message: str):
-    await create_recieve_queue(auth_uid)
-    raise NotImplementedError()
+def send_to_mq(auth_uid: str, message: Dict[str, str], conn_string: str):
+    q = QueueClient.from_connection_string(conn_string, auth_uid)
+    json_message = Message(json.dumps(message).encode("utf-8"))
+    q.send(json_message)
 
 
-async def create_recieve_queue(auth_uid: str):
-    raise NotImplementedError()
+def create_recieve_queue(auth_uid: str, conn_string: str):
+    sbc = ServiceBusClient.from_connection_string(conn_string)
+    if not sbc.create_queue(auth_uid):
+        logging.error("cannot create queue")
+        sys.exit(1)
 
 
-async def mq_pooler(ws: WebSocketResponse, auth_uid):
-    raise NotImplementedError()
+def delete_receive_queue(auth_uid: str, conn_string: str):
+    sbc = ServiceBusClient.from_connection_string(conn_string)
+    if not sbc.delete_queue(auth_uid):
+        logging.error("cannot delete queue")
+        sys.exit(1)
 
 
-def inject_config(app: web.Application, filename: str = "setting.json"):
-    with open(filename) as config_file:
-        config = json.load(config_file)
-        app.config = config
+async def wait_result(auth_uid: str, conn_string: str) -> Dict[str, str]:
+    q = QueueClient.from_connection_string(conn_string, auth_uid)
+    async with q.get_receiver() as qr:
+        messages = await qr.fetch_next(timeout=30)
+        message = str(messages[0])
+        json_message = json.loads(message)
+        return json_message
 
 
 if __name__ == "__main__":
     app = web.Application()
-    inject_config(app)
-    app.add_routes([web.get("/", send_frontend), web.post("/post", handle)])
-    web.run_app(app)
+    app.add_routes([web.post("/post", handle)])
+    web.run_app(app, port=8080, host="0.0.0.0")
+
+
+def run_sh(command: List[str]) -> str:
+    result = subprocess.run(command, capture_output=True)
+    if result.stdout:
+        return result.stdout.decode("utf-8")
+    raise Exception()
